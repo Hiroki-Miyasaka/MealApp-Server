ca44cdf263b4a4e150e03a7e3b1988eeabd09307
Hiroki <h.m.jojo.8258@gmail.com>
2023-04-25 23:58:46 -0700

    made auth controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,91 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import { createToken } from "../service/jwt.service.js";
+
+export const register = async(req, res) => {
+    const { fullName, email, password } = req.body;
+    try{
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            res.status(400).json({
+                status: "fail",
+                message: "Email already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            fullName: fullName,
+            email: email,
+            password: hashedPassword
+        });
+
+        const result = await user.save();
+
+        res.status(201).json({
+            message: "User created successfully"
+        })
+    } catch(err){
+        res.status(500).json({
+            message: "Something went wrong!",
+            err
+        })
+    }
+}
+
+export const login = async(req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({
+                status: "fail",
+                message: "Incorrect password"
+            });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordCorrect){
+            res.status(400).json({
+                status: "fail",
+                message: "Incorrect password!"
+            });
+        }
+
+        const token = createToken({id: user._id});
+
+        res.header("Authorization", token).status(200).json({
+            status: "Success",
+            token,
+            user: {
+                id: user._id,
+                fullName: user.fullName,
+                email: user.email
+            }
+        })
+    } catch(err){
+        res.status(500).json({
+            message: "Something went wrong",
+            err
+        })
+    }
+}
+
+export const logout = async(req, res) => {
+    try{
+        res.status(200).json({
+            status: "Success",
+            message: "Logged out successfully"
+        })
+    } catch(err){
+        res.status(500).json({
+            status: "fail",
+            message: "Something went wrong",
+            err
+        })
+    }
+}
